3227577d8edff5c3e2006091ef8ab31677f058c9
EllaFu17 <89587703+EllaFu17@users.noreply.github.com>
2021-12-26 16:22:41 +0800

    Add files via upload

---
+++ A1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int GetMax (int a[], int n)
+{
+    int i=0,m=a[0];
+for(i;i<n;i++)
+{
+	if(a[i+1]>m)
+	m=a[i+1];
+}
+return m;
+}
+
+int main()
+{
+	int A[] = { 2, 1, 5, 3, -1, 4, 78, 45, 0, 9};
+
+	int max,len;
+
+	len = sizeof(A) / sizeof(A[0]);
+	max = GetMax(A, len);
+	printf("max: %d", max);
+
+}
